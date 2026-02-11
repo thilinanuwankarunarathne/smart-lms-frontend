1939afcea921b367833e015f37b29716537f6843
thilina22 <test@123gmail.com>
2026-02-11 17:36:10 +0530

    up

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // This generates the /out folder
+  images: {
+    unoptimized: true, // Required for static export
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
