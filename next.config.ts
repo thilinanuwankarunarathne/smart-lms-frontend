6ddcacae67fc9b5fbdd981ba35ebd8b1271c6e6b
thilina22 <test@123gmail.com>
2026-02-11 19:05:53 +0530

    up

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Make sure output: 'export' is completely removed
-};
+const nextConfig = {};
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
